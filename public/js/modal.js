8356ba6abbfea7ff0ed60b717f5d73ad66b7d7e1
yasu0930 <yasuhon@gmail.com>
2019-11-27 02:47:53 +0900

    処理削除時のモーダル

---
+++ public/js/modal.js
@@ -0,0 +1,5 @@
+const adducePopupOpen = () => $('.delete, #adducePopupBg').show();
+const adducePopupClose = () => $('.delete, #adducePopupBg').hide();
+
+const popupOpen = () => $('.update, #popupBg').show();
+const popupClose = () => $('.update, #popupBg').hide();
