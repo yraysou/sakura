86dfa3b4b9211938ba6cea4f3f7a20f0ec3fc5e8
ryo-1123 <union.adiinoyr_18@softbank.ne.jp>
2019-12-11 20:56:33 +0900

    add role

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,7 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .sass('resources/assets/sass/base.scss', 'public/css');
-
+   .sass('resources/assets/sass/base.scss', 'public/css')
+   .options({
+      processCssUrls: false
+   });
