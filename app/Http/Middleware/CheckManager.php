072312dedbe5346104b696d2454457a204c05bce
ryo-1123 <union.adiinoyr_18@softbank.ne.jp>
2019-12-13 15:18:48 +0900

    add middleware

---
+++ app/Http/Middleware/CheckManager.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckManager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!Auth::guard('manager')->check()) {
+            return redirect()->route('manager.loginpage');
+        }else if(Auth::guard('manager')->user()->login_status == false){
+            Auth::guard('manager')->logout();
+            return redirect()->route('manager.loginpage');
+        }
+        return $next($request);
+    }
+}
